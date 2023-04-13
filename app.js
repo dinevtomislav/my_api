b22cf61babd0e5bdcc4566a0dfed5859bedbb212
Tomislav Dinev <dinevtomislav@gmail.com>
2023-04-13 17:18:29 +0100

    minor fixes that got the code working as I connected app.js and the dog_breeds.json file

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const express = require('express')
 const cors = require('cors')
-const fruits = require('./fruits.json')
+const dog_breeds = require('./dog_breeds.json')
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the dog breeds API")
+  })
+  
+  app.get('/dog_breeds', (req, res) => {
+    res.send(dog_breeds)
+  })
+
+
+
+  module.exports = app;
